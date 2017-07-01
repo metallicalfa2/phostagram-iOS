5879443ac0ef5104cec5ff425eacd69f4452cfb3
shubham rathi <shubhamrathi@icloud.com>
2017-07-01 10:51:14 +0530

    Profile page

---
+++ phostagram-ios/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  phostagram-ios
+//
+//  Created by SKIXY-MACBOOK on 30/06/17.
+//  Copyright © 2017 shubhamrathi. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+
+}
